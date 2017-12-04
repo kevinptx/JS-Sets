0a3f7d81e6f4b5ea275e8c5e8c977354ae8903d8
Linda Foo <linda.foo.dev@gmail.com>
2017-12-04 12:32:34 -0800

    Completed Problem set - No. 5 and 6

--- Set-1.js
+++ Set-1.js
@@ -1,5 +1,8 @@
+/* eslint spaces 0 */
+
 // #1
 
+/*
 console.log("'Hi my name is Chris and I'm 29 years old'");
 
 // #2
@@ -16,7 +19,7 @@ whoAmI();
 
 function whoAmI(name, age){
 
-    console.log(`Hi my ${name} is Chris and I'm ${age} years old`);
+    console.log(`Hi my ${name} and I'm ${age} years old`);
 
 }
 whoAmI();
@@ -25,7 +28,54 @@ whoAmI();
 
 function whoAmI(name, age){
   var yearOfBirth = 2017 - age;
-    console.log(`Hi my ${name} is Chris and I'm ${age} years old`);
+    console.log(`Hi my ${name} and I'm ${age}`);
     console.log(`I was born in ${yearOfBirth}`);
 }
 whoAmI();
+
+
+// #5 Move the year of birth calculation into a new function.
+
+function yearOfBirthCalc(age){
+    var yearOfBirth = 2017 - age;
+    return yearOfBirth;
+}
+
+function whoAmI(name, age){
+    var yob = yearOfBirthCalc(age);
+    console.log(`Hi my ${name} and I'm ${age}`);
+    console.log(`I was born in ${yob}`);
+}
+
+
+whoAmI("Bill", 23);
+
+*/
+
+// #6 What happens if you enter a negative age? Add a try/catch
+
+
+function yearOfBirthCalc(age){
+    var yearOfBirth = 2017 - age;
+    return yearOfBirth;
+}
+
+function whoAmI(name, age){
+    try {
+        if(age < 0) {
+           throw new Error('Age negative error')
+              }
+          } 
+          catch(e){
+          console.error(e.message);
+          }
+    var yob = yearOfBirthCalc(age);
+    console.log(`Hi my ${name} and I'm ${age}`);
+    console.log(`I was born in ${yob}`);
+
+  
+}
+whoAmI("Bill", -23);
+
+
+
