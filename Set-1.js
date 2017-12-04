5d7a92394d203f0a684c18a414222db2eadd9e92
kevinptx <kevin.peery@gmail.com>
2017-12-04 13:40:12 -0600

    3 and 4 completed

--- Set-1.js
+++ Set-1.js
@@ -6,8 +6,26 @@ console.log("'Hi my name is Chris and I'm 29 years old'");
 
 function whoAmI(){
 
-    console.log("'Hi my name is Chris and I'm 29 years old'");    
+    console.log("'Hi my name is Chris and I'm 29 years old'");
 
 }
 
+whoAmI();
+
+// #3
+
+function whoAmI(name, age){
+
+    console.log(`Hi my ${name} is Chris and I'm ${age} years old`);
+
+}
+whoAmI();
+
+// #4
+
+function whoAmI(name, age){
+  var yearOfBirth = 2017 - age;
+    console.log(`Hi my ${name} is Chris and I'm ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
+}
 whoAmI();
